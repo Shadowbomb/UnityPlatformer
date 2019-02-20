20e871c46ba021b0517f95224a544e8b8507a529
Shadowbomb <shadowbomb20@gmail.com>
2019-02-20 12:32:18 -1000

    Added animations and jumping abilities as well as grounded check

---
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformerPlayer : MonoBehaviour {
+    public float speed = 250.0f;
+    public float jumpForce = 12.0f;
+
+    private Rigidbody2D _body;
+    private Animator _anim;
+    private BoxCollider2D _box;
+
+    // Start is called before the first frame update
+    void Start() {
+        _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _box = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector2 movement = new Vector2(deltaX, _body.velocity.y);
+        _body.velocity = movement;
+
+        Vector2 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - 0.1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - 0.2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+        bool grounded = false;
+
+        if (hit != null) {
+            grounded = true;
+        }
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space)) {
+            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        if (!Mathf.Approximately(deltaX, 0)) {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+        }
+    }
+}
