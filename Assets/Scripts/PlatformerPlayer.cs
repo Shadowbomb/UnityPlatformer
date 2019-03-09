e4bd91048f841554499b5d863407de89b7a30e01
Shadowbomb <shadowbomb20@gmail.com>
2019-03-08 21:57:20 -1000

    Completed the program with following camera

--- Assets/Scripts/PlatformerPlayer.cs
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -39,9 +39,25 @@ public class PlatformerPlayer : MonoBehaviour {
             _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
+        MovingPlatform platform = null;
+        if (hit != null) {
+            platform = hit.GetComponent<MovingPlatform>();
+        }
+
+        if (platform != null) {
+            transform.parent = platform.transform;
+        } else {
+            transform.parent = null;
+        }
+
         _anim.SetFloat("speed", Mathf.Abs(deltaX));
-        if (!Mathf.Approximately(deltaX, 0)) {
-            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+
+        Vector3 pScale = Vector3.one;
+        if (platform != null) {
+            pScale = platform.transform.localScale;
+        }
+        if (deltaX != 0) {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX) / pScale.x, 1 / pScale.y, 1);
         }
     }
 }
